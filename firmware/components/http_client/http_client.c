e7629bf2e79b5bb0b43ed16545e4e4f4665a7910
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2026-04-26 01:19:34 +0200

    fix(firmware): increase HTTP response limit to 512KB
    6-color display buffer is 192KB. Previous 256KB limit was too tight
    when server adds headers/overhead or returns error pages.

--- firmware/components/http_client/http_client.c
+++ firmware/components/http_client/http_client.c
@@ -17,7 +17,7 @@
 static const char *TAG = "http_cli";
 
 /** Maximum response body size (256 KB) to prevent OOM on malicious responses */
-#define MAX_RESPONSE_SIZE (256 * 1024)
+#define MAX_RESPONSE_SIZE (512 * 1024)
 
 static char s_base_url[NVS_MAX_URL_LEN];
 static char s_mac[13];
