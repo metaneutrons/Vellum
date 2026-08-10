11d404c4bd5198aa01da0f76d932228fcdc93c47
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2026-08-11 01:01:01 +0200

    ci: support current shellcheck diagnostics

--- deploy/updater/test-update.sh
+++ deploy/updater/test-update.sh
@@ -14,11 +14,12 @@ assert version_is_newer v1.9.9 v2.0.0
 if version_is_newer v1.8.2 v1.8.2; then exit 1; fi
 if version_is_newer v2.0.0 v1.9.9; then exit 1; fi
 
-# shellcheck disable=SC2329
+# Invoked indirectly by latest_server_tag; ShellCheck versions use either code.
+# shellcheck disable=SC2317,SC2329
 github_curl() { printf '%s' '{"tag_name":"v1.8.2","draft":false,"prerelease":false}'; }
 assert test "$(latest_server_tag)" = "v1.8.2"
 
-# shellcheck disable=SC2329
+# shellcheck disable=SC2317,SC2329
 github_curl() { printf '%s' '{"tag_name":"firmware-v1.3.2","draft":false,"prerelease":false}'; }
 if latest_server_tag; then exit 1; fi
 
